62609e46deb890253ea824403ff8f8e3a8c0ebef
erikhuuki <ehuuki@naturallanguageware.com>
2024-04-05 11:19:24 -0500

    adding resources

--- src/extension.ts
+++ src/extension.ts
@@ -12,30 +12,22 @@ export function activate(context: vscode.ExtensionContext) {
 		? vscode.workspace.workspaceFolders[0].uri.fsPath : '.';
 
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "decision-trees" is now active!');
+	let treeNotes = new notes(path.join(rootPath,'NOTES'));
+	let flows = new notes(path.join(rootPath,'FLOWS'));
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('decision-trees.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from decision_trees!');
+	vscode.window.registerTreeDataProvider('tree-notes', treeNotes);
+	vscode.window.registerTreeDataProvider('tree-flows', flows);
+	
+	vscode.commands.registerCommand('tree-refresh.', (fileNode: notes) => {
+		fileNode.refresh();
 	});
-	
 
-	context.subscriptions.push(disposable);
+	let refresh = vscode.commands.registerCommand('tree-notes.refresh', (treeNotes) => {
+		// Assuming 'tree-notes' and 'tree-flows' are instances of your TreeDataProvider
+		treeNotes.refresh();
+	});
+	context.subscriptions.push(refresh);
 
-
-	vscode.window.registerTreeDataProvider('tree-notes', new notes(path.join(rootPath,'NOTES')));
-	vscode.window.registerTreeDataProvider('tree-flows', new notes(path.join(rootPath,'FLOWS')));
-	
-
-	vscode.commands.registerCommand('tree-notes.editNote', (element)=>{
-		vscode.window.showTextDocument(vscode.Uri.file(element.path));
-	})
 
 	let open_note = vscode.commands.registerCommand('notes.reveal', (fileNode) => {
 		if (/\.md$/.test(fileNode)) {
